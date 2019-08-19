1f146a761159e12d258d8fdadb1f25f72abadf3d
Rodrigo Goncalves Trevisan <rodrigo7@illinois.edu>
2019-08-19 17:56:35 -0500

    lm and svm

--- R/RF_random_CV.R
+++ R/RF_random_CV.R
@@ -21,22 +21,58 @@ regr.task = makeRegrTask(data = training_set, target = "yield")
 # this should take into accont the spatial correlation of the neighbor points:
 rdesc = makeResampleDesc("RepCV", fold = 5, reps = 5)
 
+
+
+# Create the linear model learner:
+learner.lm = makeLearner("regr.lm")
+
+# Apply the k-fold CV to fit the models to each subset:
+mod_lm = resample(learner.lm, regr.task, rdesc)
+
+# Save the predictions from the k-fold CV:
+train_pred = data.frame(mod_lm$pred)
+
+# Compare the predictions with the observed values in the test set:
+print(paste0('Correlation: ', round(cor(train_pred$truth, train_pred$response), 2)))
+print(paste0('RMSE: ', round(measureRMSE(train_pred$truth, train_pred$response), 2)))
+ggplot(train_pred, asp = 1, aes(x = response, y = truth)) + geom_point()
+ggsave('figures/train_lm.png')
+
+
+# Create the SVM learner:
+learner.svm = makeLearner("regr.svm")
+
+# Apply the k-fold CV to fit the models to each subset:
+mod_svm = resample(learner.svm, regr.task, rdesc)
+
+# Save the predictions from the k-fold CV:
+train_pred = data.frame(mod_svm$pred)
+
+# Compare the predictions with the observed values in the test set:
+print(paste0('Correlation: ', round(cor(train_pred$truth, train_pred$response), 2)))
+print(paste0('RMSE: ', round(measureRMSE(train_pred$truth, train_pred$response), 2)))
+ggplot(train_pred, asp = 1, aes(x = response, y = truth)) + geom_point()
+ggsave('figures/train_svm.png')
+
+
 # Create the RF learner, with some optimized hyperparameters:
 learner.rf = makeLearner("regr.ranger",
                          num.trees = 100,
                          min.node.size = 25)
 
 # Apply the k-fold CV to fit the models to each subset:
-mod = resample(learner.rf, regr.task, rdesc)
+mod_rf = resample(learner.rf, regr.task, rdesc)
 
 # Save the predictions from the k-fold CV:
-train_pred = data.frame(mod$pred)
+train_pred = data.frame(mod_rf$pred)
 
 # Compare the predictions with the observed values in the test set:
 print(paste0('Correlation: ', round(cor(train_pred$truth, train_pred$response), 2)))
 print(paste0('RMSE: ', round(measureRMSE(train_pred$truth, train_pred$response), 2)))
 ggplot(train_pred, asp = 1, aes(x = response, y = truth)) + geom_point()
-ggsave('figures/train.png')
+ggsave('figures/train_rf.png')
+
+
 
 # Fit a new model to all the training data:
 mod_fitted = train(learner.rf, regr.task)
